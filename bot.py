7f1ba65e66a56685e15dbd90a1d5cf0fadfd7419
fuloskop <muvaffaki@hotmail.com>
2019-09-02 07:45:12 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,59 @@
+import discord
+import asyncio
+import requests
+
+# Config:
+
+token = 'NjE3OTM4MDE3NjkyMTU1OTE2.XWyZsg.p57EufcwS8qyJOTIsKJisl5vXBg'
+apiSite = 1
+apiUrl = 'https://api.rust-servers.info/info/2625'
+
+
+client = discord.Client()
+
+# Print the starting text
+print('---------------')
+print('Discord Bot updates activity status every 2 min')
+print('---------------')
+print('Starting Bot...')
+
+
+@client.event
+async def on_ready():
+    #await client.change_presence(activity=discord.Game(name="Offline or Opening"))
+    print("Logged in as " + client.user.name)
+    print('---------------')
+    await asyncio.sleep(10)
+
+async def updateservers():
+    await client.wait_until_ready()
+    count = 0
+    while (1):
+        resp = requests.get(url=apiUrl)
+        data = resp.json()
+        online = data['online_state']
+        if online == '0':
+            await client.change_presence(activity=discord.Game(name="Offline or Opening"))
+            print('Server offline this update')
+        else:
+            players = data['players_cur']
+            maxplayers = data['players_max']
+            ip = data['ip']
+            port = data['port']
+            if count == 0:
+                await client.change_presence(activity=discord.Game(name=players + " / " + maxplayers))
+                count = 1
+                print('Server online this update with player')
+                await asyncio.sleep(100)
+            else:
+                await client.change_presence(activity=discord.Game(name=ip + ":" + port))
+                count = 0
+                print('Server online this update with ip')
+                await asyncio.sleep(10)
+            print('---------------')
+        await asyncio.sleep(10)
+
+
+# Start bot
+client.loop.create_task(updateservers())
+client.run(token)
